a50e4bd6b01debca16e20be2f389a2fe5ebe1711
Feng Guo <fengsterooni@gmail.com>
2015-06-19 22:38:24 -0700

    modified after review

--- app/src/main/java/com/udacity/android/portfolio/MainActivity.java
+++ app/src/main/java/com/udacity/android/portfolio/MainActivity.java
@@ -3,8 +3,6 @@ package com.udacity.android.portfolio;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,84 +10,36 @@ import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
     Context context;
+    static Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context = getApplicationContext();
-        setupToasts();
     }
 
-    public void setupToasts() {
-        Button button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context, "This button will launch the Sportify Streamer app!", Toast.LENGTH_SHORT).show();
-            }
-        });
+    public void displayToast(String text) {
+        if (toast != null)
+            toast.cancel();
 
-        Button button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context, "This button will launch the Scores app!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        Button button3 = (Button) findViewById(R.id.button3);
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context, "This button will launch the Library app!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        Button button4 = (Button) findViewById(R.id.button4);
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context, "This button will launch the Build It Bigger app!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        Button button5 = (Button) findViewById(R.id.button5);
-        button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context, "This button will launch the XYZ app!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        Button button6 = (Button) findViewById(R.id.button6);
-        button6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context, "This button will launch my capstone app!", Toast.LENGTH_SHORT).show();
-            }
-        });
+        toast.makeText(context, "This button will launch " + text + " App!", Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void ShowToast(View view) {
+        //Switch based on button ID
+        //Currently they all call displayToast - later they might launch intents
+        switch(view.getId()) {
+            case R.id.btnSpotify:
+            case R.id.btnScores:
+            case R.id.btnLibrary:
+            case R.id.btnBigger:
+            case R.id.btnReader:
+            case R.id.btnCapstone:
+                displayToast(((Button)view).getText().toString());
+                break;
+            default:
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
